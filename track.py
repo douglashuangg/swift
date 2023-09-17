4e969c76c9e64affbf823b48d2f07bdb7ff14fc8
Arni-Nalawade <76570969+Arni-Nalawade@users.noreply.github.com>
2023-09-16 21:47:30 -0400

    Motion tracking code
    - Code to track the selected moving object as well as figure out its co-ordinates x and y 
    - Using Opencv and python

---
+++ track.py
@@ -0,0 +1,64 @@
+import cv2
+
+#Video Source
+cap = cv2.VideoCapture(1)
+
+# mosse tracker
+#Low accuracy, high speed
+#tracker = cv2.legacy.TrackerMOSSE_create()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           b
+#High accuracy, low speed
+tracker = cv2.TrackerCSRT_create()
+
+#Bounding box selection
+success, img = cap.read()
+bbox = cv2.selectROI("Tracking",img,False)
+tracker.init(img,bbox)
+
+# constant tracking box
+def drawBox(img, bbox):
+    # Taking the distance, height and width using bbox
+    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+
+    #Finding the middle for tracking
+    success,box=tracker.update(img)
+    if success:
+        (x,y,w,h)=[int(a)for a in box]
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cx = (x + x + w) // 2
+        cy = (y + y + h) // 2
+        cv2.circle(img,(cx,cy),5,(0,0,255),-1)
+        cv2.line(img,(cx,0),(cx,480),(0,0,255),2)
+        a=int(cx)//65
+    #Display if tracking
+    cv2.putText(img, "Tracking",(75,50),cv2.FONT_HERSHEY_TRIPLEX,0.7,(0,255,0),2)
+    # X co-ordinates
+    cv2.putText(img,str((x+w)),(75,75),cv2.FONT_HERSHEY_TRIPLEX,0.7,(0,255,0),2)
+    # Y co-ordinates
+    cv2.putText(img,str((y+h)),(150,75),cv2.FONT_HERSHEY_TRIPLEX,0.7,(0,255,0),2)
+
+while True:
+    timer = cv2.getTickCount()
+    # Displaying sucess if object detected
+    success, img = cap.read()
+
+    success, bbox = tracker.update(img)
+
+    # Displaying if object is detected or lost
+    if success:
+        drawBox(img,bbox)
+    else:
+         cv2.putText(img, "Lost",(75,50),cv2.FONT_HERSHEY_TRIPLEX,0.7,(0,0,255),2)
+
+    # Getting fps
+    #fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
+    #cv2.putText(img,str(int(fps)),(75,50),cv2.FONT_HERSHEY_TRIPLEX,0.7,(0,0,255),2)
+    cv2.imshow("Tracking",img)
+
+    # Press 'q' to terminate program
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+# Program end statements
+cap.release()
+cv2.destroyAllWindows()
